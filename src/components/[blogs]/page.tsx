9f6a39a00fd74692f355692f457b6088149a3333
creativesar <uniqueluck68@gmail.com>
2025-01-03 10:45:14 +0000

    ok

--- src/components/[blogs]/page.tsx
+++ src/components/[blogs]/page.tsx
@@ -1,23 +1,24 @@
-'use client';
+'use client'
 import { usePathname } from 'next/navigation';
-import { Blog1, Blog2, Blog3, Blog4, Blog5 } from '@/app/blogfile/blogs';
+import { Blog1, Blog2, Blog3, Blog4, Blog5 , Blog6  } from '../blogs';
 
-export default function BlogPage() {
-  const pathname = usePathname();
-  const id = pathname.split('/').pop();
-
+export default function DynamicBlog() {
+  const id = usePathname();
+  console.log(id)
   const renderBlog = () => {
     switch (id) {
-      case '1':
+      case '/components/1':
         return <Blog1 />;
-      case '2':
+      case '/components/2':
         return <Blog2 />;
-      case '3':
+      case '/components/3':
         return <Blog3 />;
-      case '4':
+      case '/components/4':
         return <Blog4 />;
-      case '5':
+      case '/components/5':
         return <Blog5 />;
+        case '/components/6':
+        return <Blog6 />;
       default:
         return <p>Blog not found</p>;
     }
